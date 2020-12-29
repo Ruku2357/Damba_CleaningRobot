92abd717285ca78bae1c432c61fa1d6119a5303f
Ruku2357 <58175206+Ruku2357@users.noreply.github.com>
2020-12-30 01:28:42 +0900

    a

--- rasberry_pi/log_damba.py
+++ rasberry_pi/log_damba.py
@@ -3,7 +3,7 @@ import datetime
 
 def memo_write(word):
     now = datetime.datetime.now()
-    file_name = "/home/pi/dammemo/" + now.strftime("%Y:%m:%d_%H時") + ".txt"
+    file_name = "/home/pi/dammemo/" + now.strftime("%Y:%m:%d_%H") + ".txt"
     log_damba = open(file_name,"a")
     log_damba.write(now.strftime("%m/%d_%H:%M:%S") + "->  " + str(word))
     log_damba.close()
